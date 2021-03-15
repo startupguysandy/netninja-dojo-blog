493b3f0cb6e5101d8dbd6df5b5e7ad408a6b09bc
Andy Bradbury <andy@thestartupguys.co.uk>
2021-03-15 18:41:15 +0000

    add: implement basic styling and onClick testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-
   return (
     <div className="App">
       <Navbar />
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+	const handleClick = () => {
+		console.log('hello, ninjas!');
+	}
+
+	const handleClickAgain = (name) => {
+		console.log('Hello ' + name);
+	}
+
 	return (
 		<div className="home">
 			<h2>Homepage</h2>
+			<button onClick={handleClick}>Click me</button>
+			<button onClick={() => handleClickAgain('Andy')}>Click me again</button>
 		</div>
 	);
 }
